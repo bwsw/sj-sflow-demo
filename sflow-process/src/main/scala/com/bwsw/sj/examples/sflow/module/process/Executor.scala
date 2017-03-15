e3490f556efef4c4b70cf325719d044eb69f5927
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-03-15 12:19:19 +0700

    SJ-3400 change executors type

--- sflow-process/src/main/scala/com/bwsw/sj/examples/sflow/module/process/Executor.scala
+++ sflow-process/src/main/scala/com/bwsw/sj/examples/sflow/module/process/Executor.scala
@@ -6,25 +6,45 @@ import com.bwsw.sj.engine.core.entities.TStreamEnvelope
 import com.bwsw.sj.engine.core.environment.ModuleEnvironmentManager
 import com.bwsw.sj.engine.core.windowed.{WindowRepository, WindowedStreamingExecutor}
 import com.bwsw.sj.examples.sflow.module.process.mapreduce.Generator
+import org.apache.avro.generic.GenericData.Record
 
 import scala.collection.mutable.ArrayBuffer
 
 
-class Executor(manager: ModuleEnvironmentManager) extends WindowedStreamingExecutor[Array[Byte]](manager) {
+class Executor(manager: ModuleEnvironmentManager) extends WindowedStreamingExecutor[Record](manager) {
   private val objectSerializer = new ObjectSerializer()
   private val storage = ArrayBuffer[SflowRecord]()
 
   override def onWindow(windowRepository: WindowRepository): Unit = {
     val allWindows = windowRepository.getAll()
 
-    val envelopes = allWindows.flatMap(_._2.batches).flatMap(_.envelopes).map(_.asInstanceOf[TStreamEnvelope[Array[Byte]]])
-    val sflowRecords = envelopes.flatMap(_.data.map(bytes => {
-      val sflowRecord = objectSerializer.deserialize(bytes).asInstanceOf[SflowRecord]
-      sflowRecord.srcAs = GeoIp.resolveAs(sflowRecord.srcIP)
-      sflowRecord.dstAs = GeoIp.resolveAs(sflowRecord.dstIP)
-
-      sflowRecord
-    }))
+    val envelopes = allWindows.flatMap(_._2.batches).flatMap(_.envelopes).map(_.asInstanceOf[TStreamEnvelope[Record]])
+    val sflowRecords = envelopes.flatMap(_.data.map { avroRecord =>
+      new SflowRecord(
+        timestamp = avroRecord.get(FieldsNames.timestamp).asInstanceOf[String].toLong,
+        name = avroRecord.get(FieldsNames.name).asInstanceOf[String],
+        agentAddress = avroRecord.get(FieldsNames.agentAddress).asInstanceOf[String],
+        inputPort = avroRecord.get(FieldsNames.inputPort).asInstanceOf[String].toInt,
+        outputPort = avroRecord.get(FieldsNames.outputPort).asInstanceOf[String].toInt,
+        srcMAC = avroRecord.get(FieldsNames.srcMAC).asInstanceOf[String],
+        dstMAC = avroRecord.get(FieldsNames.dstMAC).asInstanceOf[String],
+        ethernetType = avroRecord.get(FieldsNames.ethernetType).asInstanceOf[String],
+        inVlan = avroRecord.get(FieldsNames.inVlan).asInstanceOf[String].toInt,
+        outVlan = avroRecord.get(FieldsNames.outVlan).asInstanceOf[String].toInt,
+        srcIP = GeoIp.resolveAs(avroRecord.get(FieldsNames.srcIP).asInstanceOf[String]).toString,
+        dstIP = GeoIp.resolveAs(avroRecord.get(FieldsNames.dstIP).asInstanceOf[String]).toString,
+        ipProtocol = avroRecord.get(FieldsNames.ipProtocol).asInstanceOf[String].toInt,
+        ipTos = avroRecord.get(FieldsNames.ipTos).asInstanceOf[String],
+        ipTtl = avroRecord.get(FieldsNames.ipTtl).asInstanceOf[String].toInt,
+        udpSrcPort = avroRecord.get(FieldsNames.udpSrcPort).asInstanceOf[String].toInt,
+        udpDstPort = avroRecord.get(FieldsNames.udpDstPort).asInstanceOf[String].toInt,
+        tcpFlags = avroRecord.get(FieldsNames.tcpFlags).asInstanceOf[String],
+        packetSize = avroRecord.get(FieldsNames.packetSize).asInstanceOf[String].toInt,
+        ipSize = avroRecord.get(FieldsNames.ipSize).asInstanceOf[String].toInt,
+        samplingRate = avroRecord.get(FieldsNames.samplingRate).asInstanceOf[String].toInt,
+        srcAs = avroRecord.get(FieldsNames.srcAs).asInstanceOf[String].toInt,
+        dstAs = avroRecord.get(FieldsNames.dstAs).asInstanceOf[String].toInt)
+    })
 
     storage ++= sflowRecords
   }
@@ -53,5 +73,28 @@ class Executor(manager: ModuleEnvironmentManager) extends WindowedStreamingExecu
 
 }
 
-
-
+object FieldsNames {
+  val timestamp = "timestamp"
+  val name = "name"
+  val agentAddress = "agentAddress"
+  val inputPort = "inputPort"
+  val outputPort = "outputPort"
+  val srcMAC = "srcMAC"
+  val dstMAC = "dstMAC"
+  val ethernetType = "ethernetType"
+  val inVlan = "inVlan"
+  val outVlan = "outVlan"
+  val srcIP = "srcIP"
+  val dstIP = "dstIP"
+  val ipProtocol = "ipProtocol"
+  val ipTos = "ipTos"
+  val ipTtl = "ipTtl"
+  val udpSrcPort = "udpSrcPort"
+  val udpDstPort = "udpDstPort"
+  val tcpFlags = "tcpFlags"
+  val packetSize = "packetSize"
+  val ipSize = "ipSize"
+  val samplingRate = "samplingRate"
+  val srcAs = "srcAs"
+  val dstAs = "dstAs"
+}
