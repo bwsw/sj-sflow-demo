ec5cdf97160bc03189e3b9390fcede3a1545a122
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-03-16 12:00:03 +0700

    SJ-3400 fix sflow-output

--- sflow-output/src/main/scala/com/bwsw/sj/examples/sflow/module/output/Validator.scala
+++ sflow-output/src/main/scala/com/bwsw/sj/examples/sflow/module/output/Validator.scala
@@ -6,8 +6,5 @@ import com.bwsw.sj.common.engine.StreamingValidator
   * Created by diryavkin_dn on 13.01.17.
   */
 class Validator extends StreamingValidator {
-  override def validate(options: Map[String, Any]): Boolean = {
-    options.nonEmpty
-  }
 
 }
